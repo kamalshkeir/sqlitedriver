a9b7b7cb6730fb66368a48d661b3c6ce0fb16cc9
kamalshkeir <kamalshkeir@yahoo.fr>
2023-02-19 17:39:29 +0100

    move to mattn

--- sqlitedriver.go
+++ sqlitedriver.go
@@ -1,17 +1,11 @@
 package sqlitedriver
 
 import (
-	"database/sql/driver"
-
-	"modernc.org/sqlite"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var Used bool
 
 func Use() {
 	Used = true
-}
-
-func MustRegisterScalarFunction(zFuncName string, nArg int32, xFunc func(ctx *sqlite.FunctionContext, args []driver.Value) (driver.Value, error)) {
-	sqlite.MustRegisterScalarFunction(zFuncName, nArg, xFunc)
 }
