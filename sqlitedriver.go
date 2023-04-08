39d5bfa0122e3b6de5db4a646295d20f42eda62b
kamalshkeir <kamalshkeir@yahoo.fr>
2023-04-08 19:41:05 +0200

    add driver

--- sqlitedriver.go
+++ sqlitedriver.go
@@ -1,11 +1,20 @@
 package sqlitedriver
 
 import (
+	"database/sql/driver"
+
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var Used bool
+var cd = sqlite3.SQLiteDriver{}
 
-func Use() {
+func Use() driver.Driver {
 	Used = true
+	return &cd
+}
+
+func GetDriver() driver.Driver {
+	return &cd
 }
